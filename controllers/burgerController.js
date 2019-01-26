b8a3f2d60cb316a1c25b2d2522e772ecbdd0270a
Zach Merel <zachmerel@gmail.com>
2019-01-26 10:11:58 -0600

    html pushes through but needs to be formated and non of the buttons work

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -6,5 +6,57 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+    burger.all((data) => {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", (req, res) => {
+    const newBurger = req.body;
+    burger.create([
+        "burger_name"
+    ], [
+            newBurger.burger_name
+        ], (result) => {
+            res.json({ data: result.insertId });
+        });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+    const changeDevoured = req.body;
+    burger.update({
+        devoured: changeDevoured.devoured
+    }, condition, (result) => {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, (result) => {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
 // Export routes for server.js to use.
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -4,23 +4,23 @@ const orm = require("../config/orm.js");
 
 const burger = {
     all: (cb) => {
-      orm.all("burger", (res) =>{
+      orm.all("burgers", (res) =>{
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
     create: (cols, vals, cb) =>{
-      orm.create("burger", cols, vals, (res) =>{
+      orm.create("burgers", cols, vals, (res) =>{
         cb(res);
       });
     },
     update: (objColVals, condition, cb) => {
-      orm.update("burger", objColVals, condition, (res) => {
+      orm.update("burgers", objColVals, condition, (res) => {
         cb(res);
       });
     },
     delete: (condition, cb) => {
-      orm.delete("burger", condition, (res) => {
+      orm.delete("burgers", condition, (res) => {
         cb(res);
       });
     }
